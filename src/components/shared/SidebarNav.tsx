a0fe93a1f110a563a88cdf543b7e8846a0529d48
Mohammad Aqeel <moe@blr-world.com>
2025-08-05 16:04:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/shared/SidebarNav.tsx
+++ src/components/shared/SidebarNav.tsx
@@ -83,37 +83,35 @@ export function SidebarNav() {
       <SidebarMenu className="flex-1 p-2">
         {navItems.map((item) => (
           <SidebarMenuItem key={item.href}>
-            <SidebarMenuButton
-                asChild
+            <Link href={item.href} passHref>
+              <SidebarMenuButton
                 isActive={pathname === item.href}
                 className="w-full justify-start"
                 tooltip={item.label}
               >
-                <Link href={item.href}>
                   <item.icon className="h-5 w-5" />
                   <span className="group-data-[collapsible=icon]:hidden">
                     {item.label}
                   </span>
-                </Link>
-            </SidebarMenuButton>
+              </SidebarMenuButton>
+            </Link>
           </SidebarMenuItem>
         ))}
          <Separator className="my-2" />
         {adminNavItems.map((item) => (
             <SidebarMenuItem key={item.href}>
+              <Link href={item.href} passHref>
                 <SidebarMenuButton
-                    asChild
                     isActive={pathname === item.href}
                     className="w-full justify-start"
                     tooltip={item.label}
                 >
-                    <Link href={item.href}>
-                      <item.icon className="h-5 w-5" />
-                      <span className="group-data-[collapsible=icon]:hidden">
-                      {item.label}
-                      </span>
-                    </Link>
+                    <item.icon className="h-5 w-5" />
+                    <span className="group-data-[collapsible=icon]:hidden">
+                    {item.label}
+                    </span>
                 </SidebarMenuButton>
+              </Link>
             </SidebarMenuItem>
         ))}
       </SidebarMenu>
