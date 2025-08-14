1a5101cd660cdc175a3c6686ce2966ab3de3a034
moe-dxb <moe@blr-world.com>
2025-08-14 15:21:37 +0000

    Moe

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -56,7 +56,7 @@ const Carousel = React.forwardRef<
   ) => {
     const [carouselRef, api] = useEmblaCarousel(
       {
-        ...opts,
+        ...(opts || {}),
         axis: orientation === "horizontal" ? "x" : "y",
       },
       plugins
